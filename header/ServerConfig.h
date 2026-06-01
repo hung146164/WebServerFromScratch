d157f260d1114784222ea2d9a208ca699d4a951b
hung146164 <hung146164@gmail.com>
2026-06-01 18:17:37 +0700

    change number thread

--- header/ServerConfig.h
+++ header/ServerConfig.h
@@ -3,6 +3,6 @@
 const int PORT = 8080;
 const int MAX_EVENTS = 1024;
 const int MAX_LISTEN = 1024;
-const short NUMBER_OF_WORKER = 16;
+const short NUMBER_OF_WORKER = 2;
 const char MAX_CLIENT_PER_IP = 2;
 const short CLIENT_PER_THREAD = 1000;
