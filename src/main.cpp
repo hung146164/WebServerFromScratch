9ebb5c6139c669167e43c70cda23e164bfbd9b07
hung146164 <hung146164@gmail.com>
2026-06-11 17:42:37 +0700

    done download

--- src/main.cpp
+++ src/main.cpp
@@ -20,11 +20,36 @@
 #include <filesystem>
 #include <cstdio>
 
+void test_download_image(int fd, const HttpRequest &request)
+{
+    for (auto &i : request.header)
+    {
+        std::cout << i.first << ' ' << i.second << '\n';
+    }
+    std::string_view req_range = "";
+    auto it = request.header.find("Range");
+    if (it != request.header.end())
+    {
+        req_range = it->second;
+    }
+    Http::ServeFile(fd, request.http_url, "www", req_range);
+}
+void fallback_test(int fd, const HttpRequest &request)
+{
+
+    Http::ServeFile(fd, request.http_url, "www");
+}
 int main()
 {
     ServerConfig cfg;
     cfg.port = 8081;
     cfg.num_workers = 2;
+
+    Http::Router::Register(HttpMethod::GET, "/api/download_image/testfile.img", test_download_image);
+    Http::Router::Register(HttpMethod::GET, "/api/download_image/de1.jpg", test_download_image);
+    Http::Router::Register(HttpMethod::GET, "/api/download_image/222.pdf", test_download_image);
+
+    Http::Router::RegisterFallback(fallback_test);
 
     NetworkServer server(cfg);
     server.Start();
